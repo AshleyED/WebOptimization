11b4be64622e53771feb20a52cd6491e2e556f5e
Ashley Davison <ashley.e.davison@gmail.com>
2015-12-01 21:47:01 -0500

    Update files

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,30 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        //uglify: {
-            // 2. Configuration for concatinating files goes here.
-        //}
+        uglify: {
+            build: {
+                src: 'js/perfmatters.js' ,
+                dest: 'js/perfmatters.min.js'
+            }
+        },
+
+        cssmin: {
+            target: {
+                src: 'css/style.css',
+                dest: 'css/style.min.css',
+              }
+            }
+          
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['uglify']);
 
 };
 
-uglify: {
-    build: {
-        src: 'js/perfmatters.js' ,
-        dest: 'js/perfmatters.min.js'
-    }
-}
 
 //https://24ways.org/2013/grunt-is-not-weird-and-hard#fn4987692805470b0b5865df-2
